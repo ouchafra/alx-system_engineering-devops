4c409d6eb699e87c6e7314c6ba188f85548354b0
ouchafra <ismail.soso9@gmail.com>
2024-05-08 09:53:54 -0700

    0x06-regular_expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(input)
-    regex = /hbtttn(t*)$/
-
-    match_result = input.match(regex)
-
-    puts match_result ? match_result[0] : ''
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
