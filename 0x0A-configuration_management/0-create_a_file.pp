96dc89583de92b080a5a48d593d016ce53c99d36
ouchafra <ismail.soso9@gmail.com>
2024-07-03 11:49:57 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create a file in /tmp using puppet
+
 file {'crypto_tech_coder':
-  path    =>'/tmp/school',
-  mode    =>'0744',
-  owner   =>'www-data',
-  group   =>'www-data',
-  content =>'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
