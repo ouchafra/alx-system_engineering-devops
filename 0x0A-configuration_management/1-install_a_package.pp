5cfe782f70412fb80310106aa2e284c91af39a14
ouchafra <ismail.soso9@gmail.com>
2024-07-03 16:34:01 -0700

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-nstall puppet-lint -v 2.5.2
+#install flask with puppet
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.2',
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkweug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => package['Flask'],
 }
